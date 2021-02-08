e5fe1575d7a625ae2bfdbbd2202c6538511f534d
JoeCzubiak <joeczubiak@gmail.com>
2021-02-08 15:51:14 -0800

    turned off tailwind. added favicon links

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,21 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'shadows',
+    title: 'Shadows',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { hid: 'msapplication-TileColor', name: 'msapplication-TileColor', content: '#da532c' },
+      { hid: 'theme-color', name: 'theme-color', content: '#ffffff' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', sizes: "32x32", href: '@/assets/favicons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/assets/favicons/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: "180x180", href: '/assets/favicons/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/assets/favicons/site.webmanifest' },
+      { rel: 'mask-icon', color: "#5bbad5", href: '/assets/favicons/safari-pinned-tab.svg' },
     ]
   },
 
@@ -29,7 +36,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
+    // '@nuxtjs/tailwindcss',
     '@nuxtjs/svg',
     '@nuxtjs/vuetify',
   ],
