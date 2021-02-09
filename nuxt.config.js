0f204c031e02c88fbdfa84364cf04f25bafb7ab3
JoeCzubiak <joeczubiak@gmail.com>
2021-02-09 13:30:52 -0800

    added meta data and social image

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,25 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { hid: 'msapplication-TileColor', name: 'msapplication-TileColor', content: '#da532c' },
       { hid: 'theme-color', name: 'theme-color', content: '#ffffff' },
+
+      { hid: 'og:site-name', property:'og:site_name', content: 'Shadows' },
+      { hid: 'og:type', property:'og:type', content: 'Website' },
+      { hid: 'og:title', property:'og:title', content: 'Shadows: CSS Box-Shadow Generator' },
+      { hid: 'og:description', property:'og:description', content: 'It\'s a better CSS Box-Shadow Generator. Generate multi-layered CSS box shadows. It has presets for Material Design and TailwindCSS shadows.' },
+      { hid: 'og:url', property:'og:url', content: 'https://shadows.joeczubiak.com' },
+      { hid: 'og:image', property:'og:image', content: 'https://shadows.joeczubiak.com/shadows-social-image.png' },
+      { hid: 'og:image-width', property:'og:image:width', content: '1000' },
+      { hid: 'og:image-height', property:'og:image:height', content: '523' },
+
+      { hid: 'twitter:title', property:'twitter:title', content: 'Shadows: CSS Box-Shadow Generator' },
+      { hid: 'twitter:description', property:'twitter:description', content: 'It\'s a better CSS Box-Shadow Generator. Generate multi-layered CSS box shadows. It has presets for Material Design and TailwindCSS shadows.' },
+      { hid: 'twitter:site', property:'twitter:site', content: 'Shadows' },
+      { hid: 'twitter:creator', property:'twitter:creator', content: '@joeczubiak' },
+      { hid: 'twitter:card', property:'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:image', property:'twitter:image', content: 'https://shadows.joeczubiak.com/shadows-social-image.png' },
+      
+      { hid: 'twitter:url', property:'twitter:url', content: 'https://shadows.joeczubiak.com' },
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
