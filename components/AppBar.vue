0179802bfd01cad55807e944c056751eea6baa2e
JoeCzubiak <joeczubiak@gmail.com>
2021-02-09 09:44:26 -0800

    added links to appbar

--- components/AppBar.vue
+++ components/AppBar.vue
@@ -3,9 +3,49 @@
     color="indigo"
     dense
     dark
+    class="px-14"
     >
     <v-app-bar-title>
       Shadows: A CSS Box-Shadow Generator
     </v-app-bar-title>
+    
+    <v-spacer></v-spacer>
+    <v-btn
+      color="white"
+      text
+      rounded
+      class="my-2"
+      :to="{ path: '/',hash:''}"
+      nuxt
+    >
+      Builder
+    </v-btn>
+    <v-btn
+        color="white"
+        text
+        rounded
+        class="my-2"
+        :to="{ path: '/',hash:'#presets'}"
+        nuxt
+      >
+        Presets
+      </v-btn>
+      <v-btn 
+        icon
+        href="https://github.com/jcz530/shadows"
+        rel="noopener"
+        target="_blank"
+      >
+        <IconGithub style="height: 2em; width: 2em;" class='text--white' />
+      </v-btn>
   </v-app-bar>
 </template>
+<script>
+import IconGithub from '@/assets/icons/github.svg?inline'
+
+export default {
+  components: {
+    IconGithub,
+  },
+}
+</script>
