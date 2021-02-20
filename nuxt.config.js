5739647b6f414f0f07e4644be03e27c4892102d1
JoeCzubiak <joeczubiak@gmail.com>
2021-02-19 22:13:05 -0800

    removed reference to old manifest

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,6 @@ export default {
       { rel: 'icon', type: 'image/png', sizes: "32x32", href: '@/assets/favicons/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: "16x16", href: '/assets/favicons/favicon-16x16.png' },
       { rel: 'apple-touch-icon', sizes: "180x180", href: '/assets/favicons/apple-touch-icon.png' },
-      { rel: 'manifest', href: '/assets/favicons/site.webmanifest' },
       { rel: 'mask-icon', color: "#5bbad5", href: '/assets/favicons/safari-pinned-tab.svg' },
     ]
   },
