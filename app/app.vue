1f08832bb8dfcf80cd825100c0071929a008e538
Joe Czubiak <joeczubiak@gmail.com>
2025-07-21 16:43:43 -0700

    cleanup

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,6 @@
 <script lang="ts" setup>
 import { Toaster } from '~/components/ui/sonner'
-import 'vue-sonner/style.css' // vue-sonner v2 requires this import
+import 'vue-sonner/style.css'
 </script>
 <template>
   <div class="min-h-screen bg-background">
