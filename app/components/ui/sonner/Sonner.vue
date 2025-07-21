405f6d2eed14cff06c8f6ba2fee02ba3586d6d64
Joe Czubiak <joeczubiak@gmail.com>
2025-07-21 16:36:24 -0700

    update toast style

--- app/components/ui/sonner/Sonner.vue
+++ app/components/ui/sonner/Sonner.vue
@@ -12,7 +12,15 @@ const props = defineProps<ToasterProps>()
       '--normal-bg': 'var(--popover)',
       '--normal-text': 'var(--popover-foreground)',
       '--normal-border': 'var(--border)',
-
+      '--success-bg': 'var(--popover)',
+      '--success-text': 'var(--popover-foreground)',
+      '--success-border': 'var(--border)',
     }"
   />
 </template>
+
+<style scoped>
+:deep([data-sonner-toast][data-type='success'] [data-icon]) {
+  color: var(--color-purple-600) !important;
+}
+</style>
